3447b5831494259bc5ab3010ee4f9b779991f336
Yusuf <yakhaliq01@gmail.com>
2021-06-02 17:13:51 +0100

    Challenge 11

--- lib/d_station.rb
+++ lib/d_station.rb
@@ -1,3 +1,11 @@
-class DockingStation
+require_relative 'bike'
 
+class DockingStation
+  def release_bike
+    return Bike.new
+  end
+  def dock(bike)
+    @bike = bike
+  end
+  attr_reader :bike
 end
